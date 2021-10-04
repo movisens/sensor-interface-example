c4665171a521c4d61c97cc71dd808cc355b2afe3
Ulrich Großmann <grossmann@movisens.com>
2021-10-04 16:27:33 +0200

    added led calls and firmware update

--- src/main/java/com/movisens/sensorinterface/SensorInterfaceExample.java
+++ src/main/java/com/movisens/sensorinterface/SensorInterfaceExample.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.util.List;
 
 import de.movisens.sensorinterface.ConnectorFactory;
+import de.movisens.sensorinterface.FirmwareAvailability;
 import de.movisens.sensorinterface.MeasurementConfiguration;
 import de.movisens.sensorinterface.MeasurementInformation;
 import de.movisens.sensorinterface.MeasurementStatus;
+import de.movisens.sensorinterface.ParticipantInfo;
 import de.movisens.sensorinterface.Sensor;
 
 public class SensorInterfaceExample {
@@ -21,21 +23,23 @@ public class SensorInterfaceExample {
 
 				try (sensor) {
 
-					System.out.println("sensor detected: " + sensor.getUniqueId());
-
-					System.out.println("SerialNumber: " + sensor.getSerialNumber());
-
-					System.out.println("Battery: " + sensor.getBatteryStatus() + "%");
-
+					/*
+					 * show information regarding sensor and measurement
+					 */
 					MeasurementStatus measurementStatus = sensor.getMeasurementStatus();
+					MeasurementInformation measurementInformation = sensor.getMeasurementInformation();
+					System.out.println("sensor detected: " + sensor.getUniqueId());
+					System.out.println("SerialNumber: " + sensor.getSerialNumber());
+					System.out.println("Battery: " + sensor.getBatteryStatus() + "%");
 					System.out.println("MeasurementStatus: " + measurementStatus);
+					System.out.println(measurementInformation);
 
 					if (measurementStatus == MeasurementStatus.STOPPED) {
 
-						MeasurementInformation measurementInformation = sensor.getMeasurementInformation();
-						System.out.println(measurementInformation);
-
-						if (measurementInformation.getNumWrittenSectors() > 0) {
+						/*
+						 * download measurement if available
+						 */
+						if (measurementInformation.getRecordedDuration() > 0) {
 
 							String path = "build/" + System.currentTimeMillis();
 							String measurementId = "participant0001";
@@ -43,10 +47,36 @@ public class SensorInterfaceExample {
 							sensor.saveAll(path, measurementId, p -> {
 								System.out.println(p + "%");
 							});
+
+							sensor.deleteData();
 						}
 
-						sensor.deleteData();
+						/*
+						 * update firmware if newer firmware is available
+						 */
+						if (sensor.checkFirmware() == FirmwareAvailability.FIRMWARE_NEWER_AVAILABLE) {
 
+							if (sensor.updateFirmware()) {
+								System.out.println("sensor firmware updated");
+							}
+						}
+
+						/*
+						 * switch led on and off
+						 */
+						sensor.setLedRed(true);
+						sensor.setLedRed(false);
+
+						/*
+						 * set participant for next measurement
+						 */
+						ParticipantInfo participantInfo = new ParticipantInfo();
+						participantInfo.age = 42;
+						sensor.setParticipantInfo(participantInfo);
+
+						/*
+						 * start new measurement
+						 */
 						MeasurementConfiguration measurementConfiguration = new MeasurementConfiguration();
 						measurementConfiguration.setDuration(60);
 
